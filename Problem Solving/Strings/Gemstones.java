60156d1b07a389debeb5fc0dd8b7b8f44482d741
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-05-05 10:46:31 +0530

    Create Gemstones.java

---
+++ Problem Solving/Strings/Gemstones.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the gemstones function below.
+    static int gemstones(String[] arr) {
+
+        int gems=0;
+        for(int i=0;i<26;i++)
+        {
+            char mineral=(char)(i+97);
+            int found=1;
+            for(int j=0;j<arr.length;j++)
+            {
+                if(arr[j].indexOf(mineral)==-1)
+                {
+                    found=0;
+                }
+            }
+            if(found==1)
+            {
+                gems++;
+            }
+        }
+        return gems;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String arrItem = scanner.nextLine();
+            arr[i] = arrItem;
+        }
+
+        int result = gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
